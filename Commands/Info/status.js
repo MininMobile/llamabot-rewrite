a59eb89eeeee4ed714bd37092b7ae487eb64915a
MininMobile <miningamer@gmail.com>
2017-12-08 23:07:44 +0000

    Added servers subtext

--- Commands/Info/status.js
+++ Commands/Info/status.js
@@ -9,7 +9,8 @@ exports.play = async function(i) {
     `:iphone: **Node.js Version** ${process.version}`,
     `:calling: **Discord.js Version** ${i.d.version}`,
     `:ram: **Memory Usage** ${memUsage.charAt(0) + memUsage.charAt(1) + memUsage.charAt(2) + memUsage.charAt(3)} GB / 2GB`,
-    `:stopwatch: **Uptime** ${i.f.formatSecs(Math.floor(i.b.uptime/1000))} (Days:Hours:Mins:Secs)`
+    `:stopwatch: **Uptime** ${i.f.formatSecs(Math.floor(i.b.uptime/1000))} (Days:Hours:Mins:Secs)`, "",
+    `Use \`${i.config.prefix}servers\` to list servers.`
     ];
 
     var embed = new i.d.RichEmbed()
