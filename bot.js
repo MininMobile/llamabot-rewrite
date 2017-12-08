a18e3e0ccf51c0f116096fe3a72e380699c4025c
MininMobile <miningamer@gmail.com>
2017-12-08 19:43:28 +0000

    Install D.JS + Update Bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,28 @@
+//// Install Modules
+// Init Discord Client
 const discord = require("discord.js");
+const bot = new discord.Client();
+
+// Initialize Node Modules
+const moment = require("moment");
+const fs = require("fs");
+const path = require('path');
+
+// Load Configuration
+const token = require(path.join(__dirname + "/../token.json"));
+const config = require("./config.json");
+
+//// Create Variables
+// COMMAND IMPORTER
+const imports = {
+    config:config,
+    l: function (text) { log(text) },
+    cl: function (text) { console.log(text) },
+    d:discord,
+    b:bot
+};
+
+// FUNCTIONS
+function log(text) {
+    console.log(moment().format('LTS') + ' | ' + text);
+}
