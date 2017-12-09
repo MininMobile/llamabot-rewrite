3438842784b38bbdb324ac762a60dc4ffaef7802
MininMobile <miningamer@gmail.com>
2017-12-09 21:03:53 +0000

    Create Modular System +comments

--- bot.js
+++ bot.js
@@ -31,6 +31,10 @@ const imports = {
 // FUNCTIONS
 function isNumeric(num) { return !isNaN(num); }
 
+function loadModule(module, i) {
+    require("./bot_modules/" + module + ".js").load(i)
+}
+
 function log(text) {
     console.log(moment().format('LTS') + ' | ' + text);
 }
@@ -43,23 +47,42 @@ bot.on("ready", async () => {
 
 // Message Received
 bot.on("message", async (message) => {
+    // Add Info to Imports
+    imports.m = message;
+
+    // Deny DM Messages
     if (message.author.dmChannel.id == message.channel.id) if (message.author.id != "176048981615312897") return;
+    
+    // Load Modules
+    loadModule("adblock", imports);
+
+    // Give Users a Sense of Pride and Accomplishment for Using Different Commands
     if (!message.content.toLowerCase().startsWith(config.prefix)) return;
 
+    // Create Reference Variables
     let cmd = message.content.split(" ")[0].substr(config.prefix.length);
     let arguments = message.content.split(" "); arguments[0] = arguments[0].substr(config.prefix.length);
 
+    // Go Thorugh all Commands
     for (let i = 0; i < config.commandLoader.length; i++) {
+        // Create Command Object from Id
         let command = config.commandLoader[i]
+        // If Regocnized Command
         if (command.name == cmd) {
+            // Pick out File Location
             let location = `${config.commandCatagories[command.catagory].location}`
             let file = `./${config.commandPrefix}${location}${command.file}${config.commandSuffix}`;
+            
+            // Ensure the Bot doesn't Die if System32 is Deleted
             if (fs.existsSync(file)) {
-                imports.m = message;
+                // Set Import Parameters
                 imports.a = arguments;
                 imports.c = cmd;
+                // Run Command
                 require(file).play(imports);
             }
+
+            // Stop the Loop from Running
             break
         }
     }
