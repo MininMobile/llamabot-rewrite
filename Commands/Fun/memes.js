d1854a0cce8553aa41104433e53e58138ed98231
MininMobile <miningamer@gmail.com>
2017-12-10 16:36:56 +0000

    wow

--- Commands/Fun/memes.js
+++ Commands/Fun/memes.js
@@ -1,20 +1,14 @@
 exports.play = async function(i) {
     const reddit = require("fetch-subreddit")
-    var memes;
 
     reddit.fetchSubreddit("me_irl").then((urls) => {
-        /*
-        the var urls now has the value of:
+        var memes = urls[0].urls;
+        var meme = memes[i.f.r(0, memes.length)]
+        
+        var embed = new i.d.RichEmbed()
+            .setImage(meme)
+            .setDescription(`Meme courtesy of [r/me_irl](https://reddit.com/r/me_irl)`);
 
-        [ { subreddit: 'me_irl',
-            urls:
-            [ ... ]
-        } ]
-        */
-        memes = urls[0].urls;
+        i.m.channel.send(embed)
     });
-
-    var meme = memes[i.f.r(0, memes.length)]
-    
-    i.m.reply(meme)
 }
