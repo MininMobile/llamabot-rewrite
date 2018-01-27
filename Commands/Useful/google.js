2ba4219ab58c8f86dbf0a8161bbf321a665c54af
MininMobile <miningamer@gmail.com>
2018-01-27 19:11:42 +0000

    Add thumbnail to Google

--- Commands/Useful/google.js
+++ Commands/Useful/google.js
@@ -1,34 +1,46 @@
 exports.play = async function(i) {
-	var googleSearch = new require("google-search")({
+	var gs = new require("google-search");
+	var gi = new require("google-images");
+
+	var googleSearch = new gs({
 		key: 'AIzaSyAzW8u08AJE9ur-ByoJAYTEDFdb0JAJhSA',
 		cx: '007225782620738562910:eokdvs27v7o'
 	});
+	var googleImageSearch = new gi("007225782620738562910:eokdvs27v7o", "AIzaSyAzW8u08AJE9ur-ByoJAYTEDFdb0JAJhSA");
 
 	if (i.a[1] != null) {
 		i.a.shift();
-		var words = join(" ");
+		var words = i.a.join(" ");
 
 		googleSearch.build({
 			q: words,
 			start: 1
 		}, function(error, response) {
-			let resultTitle;
-			let resultLink;
-
-			if (response.searchInformation.totalResults == 0) {
-				resultTitle = 'No results for***' + words + '***';
-				resultLink = '[...]';
-			} else {
-				resultTitle = response.items[0].title;
-				resultLink = response.items[0].link;
+			if (error) {
+				i.m.reply("Whoopsie Doopsie!");
+				return;
 			}
 
-			let embed = new i.d.RichEmbed()
-				.addField("Google Search Query", words)
-				.addField("Result", resultTitle)
-				.addField("Link", resultLink);
-
-			i.m.channel.send(embed);
+			googleImageSearch.search(words).then(images => {
+				let resultTitle;
+				let resultLink;
+	
+				if (response.searchInformation.totalResults == 0) {
+					resultTitle = 'No results for ***' + words + '***';
+					resultLink = '[   . . .   ]';
+				} else {
+					resultTitle = response.items[0].title;
+					resultLink = response.items[0].link;
+				}
+	
+				let embed = new i.d.RichEmbed()
+					.setThumbnail(images[0].url)
+					.addField("Google Search Query", words)
+					.addField("Result", resultTitle)
+					.addField("Link", resultLink);
+	
+				i.m.channel.send(embed);
+			});
 		});
 	} else {
 		i.m.reply("Please enter an argument!");
