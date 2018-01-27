f22e3683885327d79b3f1c39f82298ac388edb91
MininMobile <miningamer@gmail.com>
2018-01-27 22:32:23 +0000

    optimize variables

--- Commands/Fun/rockPaperScissors.js
+++ Commands/Fun/rockPaperScissors.js
@@ -1,39 +1,39 @@
 exports.play = async function(i) {
 	if (i.a.length == 2) {
-		const options = [`Rock!`, `Paper!`, `Scissors!`];
-		let botChoice = i.f.r(0, 3);
-		let userChoice;
+		const o = [`Rock!`, `Paper!`, `Scissors!`];
+		let bC = i.f.r(0, 3);
+		let uC;
 
 		// Get User Input
 		if (i.a[1].toLowerCase() == `gun`) {
 			i.m.channel.send(`*dies lol*`);
 			return;
 		} else if (i.a[1].toLowerCase() == `rock`)
-			userChoice = 0;
+			uC = 0;
 		else if (i.a[1].toLowerCase() == `paper`)
-			userChoice = 1;
+			uC = 1;
 		else if (i.a[1].toLowerCase() == `scissors`)
-			userChoice = 2;
+			uC = 2;
 		else
-			userChoice = -1;
+			uC = -1;
 
 		// Get Winner/Loser
-		if (userChoice == botChoice)
-			i.m.channel.send(`${options[botChoice]}, Draw...`);
-		else if (userChoice < 0)
+		if (uC == bC)
+			i.m.channel.send(`${o[bC]}, Draw...`);
+		else if (uC < 0)
 			i.m.channel.send(`Hacker! You can't choose that!`);
-		else if (userChoice == 2 && botChoice == 0)
-			i.m.channel.send(`${options[botChoice]}, I win!`);
-		else if (userChoice == 2 && botChoice == 1)
-			i.m.channel.send(`${options[botChoice]}, You win!`);
-		else if (userChoice == 1 && botChoice == 0)
-			i.m.channel.send(`${options[botChoice]}, You win!`);
-		else if (userChoice == 1 && botChoice == 2)
-			i.m.channel.send(`${options[botChoice]}, I win!`);
-		else if (userChoice == 0 && botChoice == 1)
-			i.m.channel.send(`${options[botChoice]}, I win!`);
-		else if (userChoice == 0 && botChoice == 2)
-			i.m.channel.send(`${options[botChoice]}, You win!`);
+		else if (uC == 2 && bC == 0)
+			i.m.channel.send(`${o[bC]}, I win!`);
+		else if (uC == 2 && bC == 1)
+			i.m.channel.send(`${o[bC]}, You win!`);
+		else if (uC == 1 && bC == 0)
+			i.m.channel.send(`${o[bC]}, You win!`);
+		else if (uC == 1 && bC == 2)
+			i.m.channel.send(`${o[bC]}, I win!`);
+		else if (uC == 0 && bC == 1)
+			i.m.channel.send(`${o[bC]}, I win!`);
+		else if (uC == 0 && bC == 2)
+			i.m.channel.send(`${o[bC]}, You win!`);
 		else
 			i.m.channel.send(`Something bad happened! Maybe you cheated?`);
 	} else {
