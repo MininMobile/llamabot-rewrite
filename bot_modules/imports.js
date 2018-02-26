4390431c811b858128b2a97ca65f81f97a4fa708
MininMobile <miningamer@gmail.com>
2018-02-26 17:12:38 +0000

    Set them bois

--- bot_modules/imports.js
+++ bot_modules/imports.js
@@ -1,6 +1,23 @@
 class Imports {
 	constructor(imports) {
-		this = imports;
+		this.config = imports.config
+		this.v = {
+			guilds_adblock:imports.v.guilds_adblock
+		}
+		this.f = {
+			l:imports.f.l,
+			cl:imports.f.cl,
+			lm:imports.f.lm,
+			r:imports.f.r,
+			formatSecs:imports.f.formatSecs,
+			removeArrayObject:imports.f.removeArrayObject,
+			isNumeric:imports.f.isNumeric
+		}
+		this.bf = {
+			adblock:imports.bf.adblock
+		}
+		this.d = imports.d;
+		this.b = imports.b;
 	}
 }
 
