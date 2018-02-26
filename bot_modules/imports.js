8172b18fd5d215ebb9e01ca7f67b15c7a2a7cfff
MininMobile <miningamer@gmail.com>
2018-02-26 17:22:20 +0000

    (imports.bf);

--- bot_modules/imports.js
+++ bot_modules/imports.js
@@ -3,7 +3,7 @@ class Imports {
 		this.config = imports.config
 		this.v = new require("./ImportSubClasses/data")(imports.v);
 		this.f = new require("./ImportSubClasses/functions")(imports.f);
-		this.bf = new require("./ImportSubClasses/botFunctions")
+		this.bf = new require("./ImportSubClasses/botFunctions")(imports.bf);
 		this.d = imports.d;
 		this.b = imports.b;
 	}
