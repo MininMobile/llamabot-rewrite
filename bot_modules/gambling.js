b31f9cb14673f780dfee773084dd47d4147fc5d9
MininMobile <notminin@gmail.com>
2018-07-07 20:38:44 +0200

    add flip

--- bot_modules/gambling.js
+++ bot_modules/gambling.js
@@ -34,7 +34,28 @@ Gambling.AddCommand("roll", (message, args, bot) => {
 	}
 
 	let roll = af.randomInt(1, sides);
-	message.channel.send(`You rolled a ${roll}!`)
+	message.channel.send(`You rolled a ${roll}!`);
+});
+
+Gambling.AddCommand("flip", (message, args, bot) => {
+	let sides = [
+		"src/img/heads.png",
+		"src/img/tails.png"
+	];
+
+	let sideNames = [
+		"heads",
+		"tails"
+	];
+
+	let side = af.randomInt(1, 2);
+
+	let embed = new Discord.RichEmbed()
+		.attachFile(new Discord.Attachment(sides[side-1], "coin.png"))
+		.setDescription(`You've flipped a **${sideNames[side-1]}**!`)
+		.setImage("attachment://coin.png");
+
+	message.channel.send(embed);
 });
 
 module.exports = exports = Gambling;
