e68f6a020ab104497b4ca1c56daa7d428b372cea
MininMobile <notminin@gmail.com>
2018-07-07 14:22:49 +0200

    update to opal standards

--- bot.js
+++ bot.js
@@ -1,25 +1,28 @@
-//// Install Modules
-// Initialize Discord Client
+//// import modules
 const discord = require("discord.js");
-const bot = new discord.Client();
-
-// Load Node Moduless
-const af = require("minin-api-additionalfunctions");
 const moment = require("moment");
 const fs = require("fs");
+const config = require("./config.json");
+const af = require("minin-api-additionalfunctions");
 const path = require('path');
-// Load Bot Modules
+
+//// variables
+var Commands = {};
+
+//// initialize
+// discord.js
+const Bot = new Discord.Client();
+
+// moment
+Moment.locale();
+
+// load bot modules
 const adblock = loadModule("adblock")
 
-//// c o n f i g u r e
-const token = require(path.join(__dirname + "/../token.json"));
-const config = require("./config.json");
-
-//// Initialize Modules
-// Moment
+// moment
 moment.locale();
 
-// Make Imports
+// create imports
 const imports = {
 	config:config,
 	v:{
@@ -41,8 +44,7 @@ const imports = {
 	b:bot
 };
 
-//// Create Variables
-// Functions
+//// functions
 function isNumeric(num) { return !isNaN(num); }
 
 function loadModule(module) {
@@ -53,70 +55,55 @@ function log(text) {
 	console.log(moment().format('LTS') + ' | ' + text);
 }
 
-//// Events
-// Bot Connected
+//// events
+// bot connect
 bot.on("ready", async () => {
 	imports.v.guilds_adblock = require("./json/guilds_adblock.json");
 	log(`Connected to ${bot.guilds.size} Servers`)
 });
 
-// Guild Added
+// guild joined
 bot.on("guildCreate", async (guild) => {
 	
 });
 
-// Guild Removed
+// guild left
 bot.on("guildDelete", async (guild) => {
 	adblock.on("guildDelete", imports)
 });
 
-// Message Received
+// message recieved
 bot.on("message", async (message) => {
-	// Add Info to Imports
 	imports.m = message;
 
-	// Deny DM Messages
 	if (message.author.dmChannel != null) if (message.author.id != "176048981615312897") return;
 	
-	// Load Modules
 	adblock.on("message", imports);
 	
-	// Give Users a Sense of Pride and Accomplishment for Using Different Commands
 	if (!message.content.toLowerCase().startsWith(config.prefix)) return;
 
-	// Create Reference Variables
 	let cmd = message.content.split(" ")[0].substr(config.prefix.length);
 	let arguments = message.content.split(" "); arguments[0] = arguments[0].substr(config.prefix.length);
 
-	// Go Through all Commands
 	for (let i = 0; i < config.commandLoader.length; i++) {
-		// Create Command Object from Id
 		let command = config.commandLoader[i]
-		// If Regocnized Command
 		if (command.name == cmd) {
-			// Pick out File Location
 			let location = `${config.commandCatagories[command.catagory].location}`
 			let file = `./${config.commandPrefix}${location}${command.file}${config.commandSuffix}`;
 			
-			// Ensure the Bot doesn't Die if System32 is Deleted
 			if (fs.existsSync(file)) {
-				// Set Import Parameters
-				imports
-				// Run Command
 				require(file).play(imports);
 			}
 
-			// Stop the Loop from Running
-			break
+			break;
 		}
 	}
 });
 
-//// Autosave
+//// autosave
 var autosave = setInterval(function() {
-	// Save Settings
 	adblock.on("save", imports)
 }, 600000);
 
-//// Connect Bot
-bot.login(token.token);
+//// connect
+bot.login(require("./token.json").token);
