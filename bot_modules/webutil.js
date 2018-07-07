dc3a4084d43a91fc14fb6e8771ad8247525ad5df
MininMobile <notminin@gmail.com>
2018-07-07 23:19:56 +0200

    add google webutil remove one entire folder of legacy shit :D

--- bot_modules/webutil.js
+++ bot_modules/webutil.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const IsOnline = require("is-reachable");
 const Command = require("./framework");
 const config = require("../config.json");
+const gs = new require("google-search");
+const gi = new require("google-images");
 
 const Webutil = new Command();
 
@@ -18,7 +20,47 @@ Webutil.AddCommand("online", (message, args, bot) => {
 			}
 		});
 	} else {
-		message.reply(`Enter a URL to ping, eg. \`${config.prefix}${args[0]} duckduckgo.com\``);
+		message.reply(`enter a URL to ping, eg. \`${config.prefix}${args[0]} duckduckgo.com\``);
+	}
+});
+
+Webutil.AddCommand("google", (message, args, bot) => {
+	const GoogleSearch = new gs({ key: "AIzaSyAzW8u08AJE9ur-ByoJAYTEDFdb0JAJhSA", cx: "007225782620738562910:eokdvs27v7o" });
+	const GoogleImageSearch = new gi("007225782620738562910:eokdvs27v7o", "AIzaSyAzW8u08AJE9ur-ByoJAYTEDFdb0JAJhSA");
+
+	if (args.length > 1) {
+		args.shift();
+		let words = args.join(" ");
+
+		GoogleSearch.build({
+			q: words,
+			start: 1
+		}, (err, res) => {
+			if (err) return message.channel.send(`ERROR: ${e}`);
+
+			try {
+				GoogleImageSearch.search(words).then((images) => {
+					let embed = new Discord.RichEmbed();
+		
+					if (res.searchInformation.totalResults == 0 || images.length == 0) {
+						embed
+							.attachFile(new Discord.Attachment("src/img/guess ill die.jpg", "die.jpg"))
+							.setDescription(`No results for **${words}**`)
+							.setThumbnail("attachment://die.jpg");
+					} else {
+						embed
+							.setDescription(`[${res.items[0].title}](${res.items[0].link})`)
+							.setThumbnail(images[0].url);
+					}
+		
+					message.channel.send(embed);
+				});
+			} catch (e) {
+				message.channel.send(`ERROR: ${e}`);
+			}
+		});
+	} else {
+		message.reply(`enter something to search, eg. \`${config.prefix}${args[0]} I have a lump on my foot\``);
 	}
 });
 
