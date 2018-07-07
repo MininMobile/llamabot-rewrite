3fb4765fef834e481e832bd45762fac92ae2d19d
MininMobile <notminin@gmail.com>
2018-07-07 19:44:39 +0200

    add other math functions

--- bot_modules/math.js
+++ bot_modules/math.js
@@ -58,12 +58,42 @@ Mathc.AddCommand("divide", (message, args, bot) => {
 	message.channel.send(r);
 });
 
-function va(a) {
+Mathc.AddCommand("squareroot", (message, args, bot) => {
+	if (!va(args, 1)) return message.reply(`Invalid use of \`${config.prefix}${args[0]}\`, use like \`${config.prefix}${args[0]} 36\``);
+
+	message.channel.send(Math.sqrt(parseInt(args[1])));
+});
+
+Mathc.AddCommand("perimeter", (message, args, bot) => {
+	if (!va(args)) return message.reply(`Invalid use of \`${config.prefix}${args[0]}\`, use like \`${config.prefix}${args[0]} 8 4\``);
+
+	let num1 = parseInt(args[1]);
+	let num2 = parseInt(args[2]);
+
+	let dim = `**Dimensions** ${num1}cm x ${num2}cm`
+	let peri = `**Perimeter** ${(num1 + num2) * 2}cm`
+
+	message.channel.send(`${dim}\n${peri}`);
+});
+
+Mathc.AddCommand("area", (message, args, bot) => {
+	if (!va(args)) return message.reply(`Invalid use of \`${config.prefix}${args[0]}\`, use like \`${config.prefix}${args[0]} 8 4\``);
+
+	let num1 = parseInt(args[1]);
+	let num2 = parseInt(args[2]);
+
+	let dim = `**Dimensions** ${num1}cm x ${num2}cm`
+	let area = `**Area** ${num1 * num2}cm²`
+
+	message.channel.send(`${dim}\n${area}`);
+});
+
+function va(a, expected = 2) {
 	let args = a.slice(1, a.length);
 
 	r = true;
 
-	if (args.length < 2) r = false;
+	if (args.length < expected) r = false;
 	args.forEach((num) => { if (isNaN(parseInt(num))) r = false; });
 
 	return r;
