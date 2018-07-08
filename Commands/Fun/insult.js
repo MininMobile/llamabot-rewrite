0ae2a0178e3575c621acb1949fafd169aa49dfc6
MininMobile <notminin@gmail.com>
2018-07-08 11:04:03 +0200

    add insult

--- Commands/Fun/insult.js
+++
@@ -1,5 +0,0 @@
-exports.play = async function(i) {
-	let r = require("./../../json/insults.json");
-
-	i.m.channel.send(r[i.f.r(0,r.length)])
-}
--- bot_modules/mocking.js
+++ bot_modules/mocking.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const af = require("minin-api-additionalfunctions");
 const Command = require("./framework");
 
 const Mocking = new Command();
@@ -14,6 +15,25 @@ Mocking.AddCommand("chkn", (message, args, bot) => {
 	}
 
 	message.channel.send(result, { file: "src/img/cock.png" });
+});
+
+Mocking.AddCommand("toxic", (message, args, bot) => {
+	let r = [
+		"slob on me nobe",
+		"suck me",
+		"lol gay",
+		"kiss your sister",
+		"u have the big gay",
+		"no u",
+		"consider death",
+		"man fuck you",
+		"suck my dick",
+		"inhale my dong",
+		"y-y-your mum :sunglasses:",
+		"no suck, suck is dead"
+	];
+
+	message.channel.send(r[af.randomInt(0, r.length)]);
 });
 
 module.exports = exports = Mocking;
